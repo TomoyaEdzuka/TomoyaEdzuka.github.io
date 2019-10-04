dbc0723e6e19857e712146d8bf73179e08c450be
TomoyaEdzuka <eduka.tomoya@j.mbox.nagoya-u.ac.jp>
2019-10-04 10:42:46 +0900

    updated

---
+++ date_update.js
@@ -0,0 +1,5 @@
+var modified = new Date(document.lastModified);
+var year = modified.getFullYear();
+var month = modified.getMonth() + 1;
+var date = modified.getDate();
+document.write("Last update：" + year + "-" + month + "-" + date);
